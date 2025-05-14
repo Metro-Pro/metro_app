946f11c9a05817c505f63682c207a71162fcfd03
PeterBotros <pepobemen504@gmail.com>
2025-05-14 19:45:32 +0300

    adding profile file and footer code

--- lib/view/pages/home.dart
+++ lib/view/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:metro_app/view/pages/profile.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -29,7 +30,12 @@ class HomeScreen extends StatelessWidget {
                       children: [
                         GestureDetector(
                           onTap: () {
-                            // Profile icon tapped
+                            // Handle back button tap
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Profile()),
+                            );
                           },
                           child: Container(
                             padding: const EdgeInsets.all(8),
@@ -44,12 +50,6 @@ class HomeScreen extends StatelessWidget {
                             ),
                           ),
                         ),
-                        // SvgPicture.asset(
-                        //   "assets/images/Group 4.svg",
-                        //   width: 20.0,
-                        //   height: 19.0,
-                        //   fit: BoxFit.contain,
-                        // ),
                         const SizedBox(
                             width:
                                 30.0), // Placeholder for alignment or a future button
@@ -58,7 +58,7 @@ class HomeScreen extends StatelessWidget {
                   ),
                   Column(
                     children: [
-                      const SizedBox(height: 1030),
+                      const SizedBox(height: 750),
                       SingleChildScrollView(
                         scrollDirection: Axis.horizontal,
                         child: Row(
@@ -70,15 +70,50 @@ class HomeScreen extends StatelessWidget {
                           ],
                         ),
                       ),
-                      const SizedBox(height: 20),
-                      const SizedBox(height: 20),
-                      const SizedBox(height: 20),
+                      
                     ],
                   ),
                 ],
               ),
             ),
           )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromRGBO(
+            33, 33, 33, 1.0), // Adjust color and opacity as needed
+        selectedItemColor: const Color.fromRGBO(255, 255, 255, 1.0),
+        unselectedItemColor: const Color.fromRGBO(215, 255, 255, 1.0),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: const Color.fromRGBO(255, 255, 255, 1.0),
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.payments_outlined,
+              color: const Color.fromRGBO(255, 255, 255, 1.0),
+            ),
+            label: 'payment',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.card_membership_outlined,
+              color: const Color.fromRGBO(255, 255, 255, 1.0),
+            ),
+            label: 'ticket',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.train_outlined,
+              color: const Color.fromRGBO(255, 255, 255, 1.0),
+            ),
+            label: 'metro line',
+          ),
         ],
       ),
     );
---
+++ lib/view/pages/profile.dart
@@ -0,0 +1,127 @@
+import 'package:metro_app/view/pages/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:metro_app/view/pages/login.dart';
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Profile',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(
+                255, 255, 255, 1), // Corrected font color property
+          ),
+          textAlign: TextAlign.center, // Fixed text alignment
+        ),
+        backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => HomeScreen()),
+            );
+          },
+        ),
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          // Background SVG
+          Positioned.fill(
+            child: SvgPicture.asset(
+              "assets/images/Group 5.svg",
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          // Profile Content
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 50), // Spacer to push content down
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+                  // backgroundImage:
+                  //     AssetImage("assets/images/profile_picture.png"),
+                ),
+                SizedBox(height: 12),
+                Text(
+                  "username",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Text(
+                  "username@example.com",
+                  style: TextStyle(fontSize: 16, color: Colors.white70),
+                ),
+                SizedBox(height: 20),
+
+                // Profile Options
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(20)),
+                    ),
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading: Icon(Icons.settings, color: Colors.blue),
+                          title: Text("edit profile"),
+                          trailing: Icon(Icons.arrow_forward_ios),
+                          onTap: () {},
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.settings, color: Colors.blue),
+                          title: Text("Settings"),
+                          trailing: Icon(Icons.arrow_forward_ios),
+                          onTap: () {
+                            // Navigator.pushReplacement(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //       builder: (context) => HomeScreen()),
+                            // );
+                          },
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.lock, color: Colors.red),
+                          title: Text("Privacy"),
+                          trailing: Icon(Icons.arrow_forward_ios),
+                          onTap: () {},
+                        ),
+                        ListTile(
+                          leading: Icon(Icons.logout, color: Colors.black),
+                          title: Text("Logout"),
+                          trailing: Icon(Icons.arrow_forward_ios),
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(builder: (context) => Login()),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
