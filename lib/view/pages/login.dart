147f68dd24c11f028fd9a6699d9397be29bde3b7
PeterBotros <pepobemen504@gmail.com>
2025-05-14 17:06:07 +0300

    adding home, login, signup files

--- lib/view/pages/login.dart
+++ lib/view/pages/login.dart
@@ -1,4 +1,4 @@
-import 'package:railway_system/view/pages/home.dart';
+import 'package:metro_app/view/pages/home.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
