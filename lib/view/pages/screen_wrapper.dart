9ae0ab9ece3f205a469852648e26b536871e0865
PeterBotros <pepobemen504@gmail.com>
2025-05-30 18:44:55 +0300

    payment after pruache5

--- lib/view/pages/screen_wrapper.dart
+++ lib/view/pages/screen_wrapper.dart
@@ -14,7 +14,7 @@ class ScreenWrapper extends StatefulWidget {
 class _ScreenWrapperState extends State<ScreenWrapper> {
   List<Widget> pages = [
     const HomeScreen(),
-    const PaymentPage(),
+    const SubscriptionPage(),
     const TicketPage(),
     const MetroLinePage()
   ];
